17449520dabce57b017c37f7a6d917afbb5667a8
hokageinfinity <nateandjess1218@gmail.com>
2026-06-25 00:07:27 -0400

    Add array of names to names.js
    Added an array of names to the names.js file.

--- data/names.js
+++ data/names.js
@@ -1,1 +1,7 @@
-
+const names = [
+"Arin", "Kara", "Leo", "Mira", "Kael", "Nora", "Jace", "Luna",
+"Damon", "Elara", "Rin", "Axel", "Nova", "Kai", "Aiden",
+"Sora", "Yuki", "Hiro", "Rei", "Zane", "Iris", "Ryu",
+"Novae", "Kira", "Taro", "Miko", "Drake", "Sage", "Vex",
+"Lyra", "Orin", "Nyx", "Sol", "Eden", "Juno", "Atlas"
+];
